e147c7dc105afb318a577ff4865f2e1c2ce618a6
clements <clements@theodo.fr>
2024-01-25 17:52:53 +0100

    new code removal

--- app/embed/[assistantId]/page.js
+++ app/embed/[assistantId]/page.js
@@ -2,21 +2,13 @@
 import Image from "next/image";
 import { useState, useEffect, useRef } from "react";
 import OpenAI from "openai";
-import { useSearchParams } from "next/navigation";
 
-function Embed({ params: { assistantId } }) {
+function Embed({ assistantId, Okey }) {
   const [question, setQuestion] = useState("");
   const [chat, setChat] = useState([]);
   const [thread, setThread] = useState(null);
-  const [run, setRun] = useState(null);
   const [openai, setOpenai] = useState(null);
-  const [key, setKey] = useState("");
-  const [keyAdded, setKeyAdded] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [runInterval, setRunInterval] = useState(null);
-  const searchParams = useSearchParams();
-  const intervalRef = useRef(null);
-  intervalRef.current = runInterval;
   const chatRef = useRef(null);
   chatRef.current = chat;
 
@@ -35,14 +27,9 @@ function Embed({ params: { assistantId } }) {
         ...chatRef.current,
         { isBot: true, msg: messages.data[0].content[0].text.value },
       ]);
-      // clearInterval(intervalRef.current);
     } else {
       setTimeout(() => getAnswer(threadId, runId), 200);
     }
-  };
-  const addKey = () => {
-    localStorage.setItem("openAIKey", key);
-    setKeyAdded((prev) => true);
   };
   const askAssistant = async () => {
     let getQuestion = question;
@@ -57,7 +44,6 @@ function Embed({ params: { assistantId } }) {
     } else {
       getThread = thread;
     }
-    // console.log(getThread)
     await openai.beta.threads.messages.create(getThread.id, {
       role: "user",
       content: getQuestion,
@@ -65,116 +51,60 @@ function Embed({ params: { assistantId } }) {
     const getRun = await openai.beta.threads.runs.create(getThread.id, {
       assistant_id: assistantId,
     });
-    setRun(getRun);
     getAnswer(getThread.id, getRun.id);
   };
   useEffect(() => {
-    if (keyAdded == true && key != "") {
-      setOpenai(new OpenAI({ apiKey: key, dangerouslyAllowBrowser: true }));
-    }
-  }, [keyAdded]);
-  useEffect(() => {
-    let getKey = localStorage.getItem("openAIKey");
-    if (getKey != null && getKey != "") {
-      setKey(getKey);
-      setKeyAdded((prev) => true);
+    if (Okey != "") {
+      setOpenai(new OpenAI({ apiKey: Okey, dangerouslyAllowBrowser: true }));
     }
   }, []);
 
   return (
     <div className="h-screen w-screen md:p-4 flex flex-col bg-myBg gap-4">
-      <div className={`flex justify-between bg-myPrimary rounded-xl p-4`}>
-        <div className="flex items-center gap-2">
-          <Image height={25} width={25} src="/assistant.svg" alt="logo" />
-          <span className="font-semibold">myAssistant</span>
-        </div>
-        <div className="d-flex align-items-center gap-2 cursor-pointer">
-          <Image
-            height={20}
-            width={20}
-            onClick={refreshChat}
-            src="/refresh.svg"
-            alt="refresh"
-          />
-          {/* <Image height={20} width={20} onClick={closeFrame} src='/cancel.svg'/> */}
-        </div>
-      </div>
-      {keyAdded ? (
-        <>
-          <div className="flex flex-col gap-2 w-full h-full overflow-y-auto scroll">
-            {chat.map((msg, index) => (
-              <div
-                key={index}
-                className={`${
-                  msg.isBot
-                    ? "bg-gray-900 text-gray-100 self-start"
-                    : "text-gray-900 bg-gray-100 self-end border-2"
-                } rounded-lg  px-3 py-2 max-w-sm`}
-              >
-                {msg.msg}
-              </div>
-            ))}
-            {loading && (
-              <div
-                className={`bg-gray-900 text-gray-100 self-start rounded-lg  px-3 py-2 max-w-sm`}
-              >
-                <div className="flex h-4 items-center gap-2">
-                  <div className="bounce bounce1 rounded-full bg-slate-500 h-2 w-2" />
-                  <div className="bounce bounce2 rounded-full bg-slate-500 h-2 w-2" />
-                  <div className="bounce bounce3 rounded-full bg-slate-500 h-2 w-2" />
-                </div>
-              </div>
-            )}
-          </div>
-          <div className="flex gap-2 mt-auto">
-            <input
-              id="question"
-              className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-              placeholder="Ask a question"
-              required
-              value={question}
-              onKeyDown={(e) => {
-                e.code == "Enter" && !e.shiftKey && askAssistant();
-              }}
-              onChange={(e) => setQuestion(e.target.value)}
-            />
-            <button
-              onClick={askAssistant}
-              className=" bg-mySecondary hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-4 py-2.5 text-center "
-            >
-              <Image height={20} width={20} src="/send.svg" alt="send" />
-            </button>
-          </div>
-        </>
-      ) : (
-        <div className="flex max-w-xl flex-col pt-4 md:pt-0 gap-2 text-sm text-black">
-          <div className=" text-xl font-semibold mb-4">
-            Welcome to myAssistant, enter your{" "}
-            <a
-              className="underline italic"
-              href="https://platform.openai.com/api-keys"
-              target="_blank"
-            >
-              OpenAI key
-            </a>{" "}
-            to get started
-          </div>
-          <input
-            id="name"
-            className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-            placeholder="sk---------------"
-            required
-            value={key}
-            onChange={(e) => setKey(e.target.value)}
-          />
-          <button
-            onClick={() => key !== "" && addKey()}
-            className="bg-mySecondary hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+      <div className="flex flex-col gap-2 w-full h-full overflow-y-auto scroll">
+        {chat.map((msg, index) => (
+          <div
+            key={index}
+            className={`${
+              msg.isBot
+                ? "bg-gray-900 text-gray-100 self-start"
+                : "text-gray-900 bg-gray-100 self-end border-2"
+            } rounded-lg  px-3 py-2 max-w-sm`}
           >
-            Add OpenAI Key
-          </button>
-        </div>
-      )}
+            {msg.msg}
+          </div>
+        ))}
+        {loading && (
+          <div
+            className={`bg-gray-900 text-gray-100 self-start rounded-lg  px-3 py-2 max-w-sm`}
+          >
+            <div className="flex h-4 items-center gap-2">
+              <div className="bounce bounce1 rounded-full bg-slate-500 h-2 w-2" />
+              <div className="bounce bounce2 rounded-full bg-slate-500 h-2 w-2" />
+              <div className="bounce bounce3 rounded-full bg-slate-500 h-2 w-2" />
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="flex gap-2 mt-auto">
+        <input
+          id="question"
+          className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+          placeholder="Ask a question"
+          required
+          value={question}
+          onKeyDown={(e) => {
+            e.code == "Enter" && !e.shiftKey && askAssistant();
+          }}
+          onChange={(e) => setQuestion(e.target.value)}
+        />
+        <button
+          onClick={askAssistant}
+          className=" bg-mySecondary hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-4 py-2.5 text-center "
+        >
+          <Image height={20} width={20} src="/send.svg" alt="send" />
+        </button>
+      </div>
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import Image from "next/image";
-import Link from "next/link";
+import Embed from "./embed/[assistantId]/page";
 import { useContext } from "react";
 import { KeyContext } from "../components/MainComponent";
 
@@ -9,15 +9,6 @@ function Home() {
   const getKey = useContext(KeyContext);
   const [keyAdded, setKeyAdded] = useState(false);
   const [assistants, setAssistants] = useState([]);
-  let addID = async () => {
-    const response = await fetch("/api/storeID", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ id: getKey.key }),
-    }).then(() => setKeyAdded(true));
-  };
   const fetchData = async () => {
     const response = await fetch("/api");
     const data = await response.json();
@@ -39,36 +30,24 @@ function Home() {
   useEffect(() => {
     fetchData();
   }, []);
-  console.log(assistants);
   return (
-    <main className="flex min-h-screen flex-col  bg-myBg ">
+    <main className="flex min-h-screen flex-col">
       <div
         id="header"
         className="flex items-center justify-between flex-wrap gap-2 bg-slate-900 text-white px-2 md:px-8 py-4  "
       >
         <div className="flex items-center gap-2">
           <Image src="/assistant.svg" height={50} width={50} alt="logo" />
-          <h6 className="  text-3xl font-semibold">Open Custom GPT</h6>
+          <h6 className="  text-3xl font-semibold">Paperass AI</h6>
         </div>
       </div>
-      <div className=" max-w-3xl px-2 md:px-8 py-6 flex flex-col gap-5 text-gray-800">
-        {
-          <div className=" flex flex-wrap gap-4">
-            {assistants.map((assistant) => (
-              <Link href={"/embed/" + assistant.id}>
-                <div className=" border-2 px-4 py-2 flex gap-4 items-center rounded-xl h-16 min-w-[20rem] max-w-xl cursor-pointer">
-                  <div className=" rounded-full bg-slate-500 h-2 w-2" />
-                  <div className=" flex flex-col">
-                    <div className=" text-base font-medium">
-                      {assistant.name}
-                    </div>
-                  </div>
-                </div>
-              </Link>
-            ))}
-          </div>
-        }
-      </div>
+      {(getKey.key != undefined || getKey.key != "") && (
+        <div id="chat" className="flex flex-grow">
+          {assistants.length > 0 && (
+            <Embed assistantId={assistants[0].id} Okey={getKey.key} />
+          )}
+        </div>
+      )}
     </main>
   );
 }
