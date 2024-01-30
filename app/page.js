43f935923bea2ad729a1a11df1abade266390b9e
clements <clements@theodo.fr>
2024-01-30 14:52:42 +0100

    Signup page integrated in global layout

--- app/page.js
+++ app/page.js
@@ -1,15 +1,12 @@
 "use client";
 import { useState, useEffect } from "react";
-import Image from "next/image";
 import Embed from "../components/ChatComponent";
 import { useContext } from "react";
 import { KeyContext } from "../components/MainComponent";
-import Link from "next/link";
 
 function Home() {
   const getKey = useContext(KeyContext);
   const [assistant, setAssistant] = useState();
-  const [logged, setLogged] = useState(false);
   const fetchData = async () => {
     let data = undefined;
     try {
@@ -30,35 +27,11 @@ function Home() {
     fetchData();
   }, []);
   return (
-    <main className="flex flex-col h-screen">
-      <div
-        id="header"
-        className="flex items-center justify-between flex-wrap gap-2 bg-slate-900 text-white px-2 md:px-8 py-4  "
-      >
-        <div className="flex items-center gap-2">
-          <Image src="/assistant.svg" height={50} width={50} alt="logo" />
-          <h6 className="  text-3xl font-semibold">Paperass AI</h6>
-        </div>
-        {logged ? (
-          <div>bla</div>
-        ) : (
-          <div className="flex gap-5" id="login-buttons">
-            <Link
-              href="/signup"
-              className="bg-mySecondary rounded-lg p-2.5 text-slate-900"
-            >
-              S'inscrire
-            </Link>
-            <button>Se Connecter</button>
-          </div>
-        )}
-      </div>
+    <div id="chat" className="flex flex-1">
       {getKey.key && getKey.key !== "" && assistant !== undefined && (
-        <div id="chat" className="flex flex-1">
-          <Embed assistantId={assistant} Okey={getKey.key} />
-        </div>
+        <Embed assistantId={assistant} Okey={getKey.key} />
       )}
-    </main>
+    </div>
   );
 }
 
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,18 +1,46 @@
-'use client'
-import { createContext,useState } from "react"
+"use client";
+import { createContext, useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const keyDefault = {
-    key: '',
-    setKey: () => {}
-}
+  key: "",
+  setKey: () => {},
+};
 export const KeyContext = createContext(keyDefault);
 
-function MainComponent({children}) {
-    const [key,setKey] = useState("")
-    return(
-        <KeyContext.Provider value={{key,setKey}}>
-            {children}
-        </KeyContext.Provider>
-    )
+function MainComponent({ children }) {
+  const [key, setKey] = useState("");
+  const [logged, setLogged] = useState(false);
+
+  return (
+    <KeyContext.Provider value={{ key, setKey }}>
+      <main className="flex flex-col h-screen">
+        <div
+          id="header"
+          className="flex items-center justify-between flex-wrap gap-2 bg-slate-900 text-white px-2 md:px-8 py-4  "
+        >
+          <div className="flex items-center gap-2">
+            <Image src="/assistant.svg" height={50} width={50} alt="logo" />
+            <h6 className="  text-3xl font-semibold">Paperass AI</h6>
+          </div>
+          {logged ? (
+            <div>bla</div>
+          ) : (
+            <div className="flex gap-5" id="login-buttons">
+              <Link
+                href="/signup"
+                className="bg-mySecondary rounded-lg p-2.5 text-slate-900"
+              >
+                S'inscrire
+              </Link>
+              <button>Se Connecter</button>
+            </div>
+          )}
+        </div>
+        {children}
+      </main>
+    </KeyContext.Provider>
+  );
 }
-export default MainComponent
+export default MainComponent;
